4f79d16c7a03c28aa048ae5aa5e39a1538c1be33
lyric228 <xhikzerox@gmail.com>
2025-01-28 16:32:25 +0500

    One more fix

--- ip.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo ""
-echo "SERVER IP ADDRESS"
-echo "$(curl -s ifconfig.me):25565"
-echo ""
--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-java -Xmx6144M -Xms4096M -jar ./forge-1.12.2-14.23.5.2860.jar nogui
