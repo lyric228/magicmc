5aff0acee5532dfe4b7def12c141d75d8101e5a6
lyric228 <xhikzerox@gmail.com>
2025-01-28 16:31:27 +0500

    Fix

--- server/run.sh
+++ server/run.sh
@@ -5,22 +5,4 @@ echo "SERVER IP ADDRESS"
 echo "$(curl -s ifconfig.me):25565"
 echo ""
 
-# sudo ufw enable
-#
-# config_file="server.properties"
-# external_ip=$(curl -s ifconfig.me)
-#
-# if [[ -z "$external_ip" ]]; then
-#   echo "Не удалось получить внешний IP-адрес."
-#   exit 1
-# fi
-#
-# if grep -q "^server-ip=" "$config_file"; then
-#   sed -i "s/^server-ip=.*/server-ip=$external_ip/" "$config_file"
-#   echo "IP-адрес успешно обновлён: $external_ip"
-# else
-#   echo "server-ip=$external_ip" >> "$config_file"
-#   echo "Строка server-ip добавлена с IP-адресом: $external_ip"
-# fi
-
 java -Xmx6144M -Xms4096M -jar ./forge-1.12.2-14.23.5.2860.jar nogui
