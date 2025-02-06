0390a4577fdcce76eb0bd1e02acc8427f99f1f09
lyric228 <xhikzerox@gmail.com>
2025-02-06 21:57:13 +0500

    fix

--- server/run.sh
+++ server/run.sh
@@ -5,4 +5,12 @@ echo "SERVER IP ADDRESS"
 echo "$(curl -s ifconfig.me):25565"
 echo ""
 
-java -Xmx3072M -Xms3072M -jar ./forge-1.12.2-14.23.5.2860.jar nogui
+RAM_GB=$(free -g | awk '/^Mem:/{print $2}')
+RAM_MB=$((RAM_GB * 1024))
+RAM_VAR=$((RAM_MB - 2048))
+
+echo ""
+echo "USING ${RAM_VAR} MB RAM"
+echo ""
+
+java -Xmx${RAM_VAR}M -Xms${RAM_VAR}M -jar ./forge-1.12.2-14.23.5.2860.jar nogui
