7058b7deb72f71bdfb3b562af230fca7dc8777b4
lyric228 <xhikzerox@gmail.com>
2025-02-15 01:14:21 +0500

    zhopa

--- server/run.sh
+++ server/run.sh
@@ -5,18 +5,20 @@ echo "SERVER IP ADDRESS"
 echo "$(curl -s ifconfig.me):25565"
 echo ""
 
-# Получаем общую RAM + SWAP
 TOTAL_MEM=$(free -m | awk '
     /^Mem:/ { mem_total = $2 }
     /^Swap:/ { swap_total = $2 }
     END { print mem_total + swap_total }'
 )
 
-RAM_VAR=$((TOTAL_MEM - 4096))
+RAM_VAR=$((TOTAL_MEM - 8192))
 
 echo ""
 echo "AVAILABLE MEMORY (RAM+SWAP): ${TOTAL_MEM} MB"
 echo "USING ${RAM_VAR} MB FOR SERVER"
 echo ""
 
-java -Xmx${RAM_VAR}M -Xms${RAM_VAR}M -jar ./forge-1.12.2-14.23.5.2860.jar nogui
+java -Xmx${RAM_VAR}M -Xms${RAM_VAR}M \
+-XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 \
+-XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC \
+-jar ./forge-1.12.2-14.23.5.2860.jar nogui
